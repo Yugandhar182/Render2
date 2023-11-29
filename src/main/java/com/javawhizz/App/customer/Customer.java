9be001c6945fb64953b3ad612d94ef22b2695d0a
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-29 14:45:52 +0530

    Update Customer.java

--- src/main/java/com/javawhizz/App/customer/Customer.java
+++ src/main/java/com/javawhizz/App/customer/Customer.java
@@ -1,16 +1,1 @@
-package com.javawhizz.App.customer;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class Customer {
-    private Long customerId;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String email;
-}
